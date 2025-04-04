be11a3fab4b35c895876911ced70b38549f024a4
siva0704 <vaibhavabg1234@gmail.com>
2025-04-04 21:16:18 +0530

    vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: process.env.VITE_BASE_PATH || "/School",
+  base: 'School/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
